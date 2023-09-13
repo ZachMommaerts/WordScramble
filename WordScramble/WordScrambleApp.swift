041e80b61d61b7cd1b25fe740e8c5e554a5fb4f0
ZachMommaerts <zmommaerts@gmail.com>
2023-09-13 05:22:39 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Zach Mommaerts on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
